f6d8037b62c1089984075f5411e81e704bcd81b1
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-08-17 12:03:38 -0400

    update chat box component

--- chat-app/src/components/ChatBox.jsx
+++ chat-app/src/components/ChatBox.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import SendMessage from "./SendMessage";
+import Message from "./Message";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
 
 function ChatBox() {
+  const [chatTexts, setChatTexts] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const fetchedMessages = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setChatTexts(sortedMessages);
+    });
+    return () => unsubscribe;
+  }, []);
+
   return (
-    <div>ChatBox</div>
-  )
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {chatTexts?.map((chatText) => (
+          <Message key={chatText.id} chatText={chatText} />
+        ))}
+      </div>
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} />
+    </main>
+  );
 }
 
-export default ChatBox
+export default ChatBox;
