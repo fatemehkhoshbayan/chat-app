8e55a0090717d9f6513b3e5d62908aaff64f521b
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2023-08-17 12:03:12 -0400

    Retrive message from firebase

--- chat-app/src/components/Message.jsx
+++ chat-app/src/components/Message.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-function Message() {
+function Message({ chatText }) {
+  const[user] = useAuthState(auth);
+
   return (
-    <div>Message</div>
-  )
+    <div className={`chat-bubble ${chatText.uid === user.uid ? "right" : ""}`}>
+      <img
+        className="chat-bubble__left"
+        src={chatText.avatar}
+        alt="user avatar"
+      />
+      <div className="chat-bubble__right">
+        <p className="user-name">{chatText.name}</p>
+        <p className="user-message">{chatText.text}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Message
+export default Message;
